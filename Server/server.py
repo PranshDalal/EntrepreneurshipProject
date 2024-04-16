9c083af15a3443f7550eb12c00bbfe09441de23e
PranshDalal <121412920+PranshDalal@users.noreply.github.com>
2024-04-16 07:15:50 -0700

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,1 +1,16 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
+@app.route("/api/message", methods=['GET'])
+def helloworld():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
