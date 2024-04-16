b666376466c055cadf1f608f84c99d610cd5c601
PranshDalal <121412920+PranshDalal@users.noreply.github.com>
2024-04-16 07:12:52 -0700

    Add files via upload

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,1 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/message')
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{message}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
