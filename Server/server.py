1d9903cf5365dbd0c02a1703eb6cf3f487cf2b47
PranshDalal <121412920+PranshDalal@users.noreply.github.com>
2024-04-18 07:09:14 -0700

    Literally like so cool

--- Server/server.py
+++ Server/server.py
@@ -1,13 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "http://localhost:3001"]}})
 
 @app.route("/api/message", methods=['GET'])
 def helloworld():
-    return "Hello World!"
+    return jsonify({"message": "Hello World!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(debug=True, port=3001)
