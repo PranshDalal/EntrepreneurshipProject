eb33cb918b13405b697fff8ef0bcb56e66a84391
PranshDalal <121412920+PranshDalal@users.noreply.github.com>
2024-04-26 20:08:30 -0700

    fixed nathan

--- Server/models.py
+++ Server/models.py
@@ -1,13 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from uuid import uuid4
- 
+import uuid
+
 db = SQLAlchemy()
- 
-def get_uuid():
-    return uuid4().hex
- 
+
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.String(11), primary_key=True, unique=True, default=get_uuid)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.Text, nullable=False)
